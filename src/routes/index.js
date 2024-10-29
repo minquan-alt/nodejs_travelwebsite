b8615f6abd0460ce1eead592fe5e578a371988bf
minquan-alt <gameonlinepa@gmail.com>
2024-10-29 15:15:01 +0700

    install prettier lint-staged husky

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+const searchRouter = require('./search');
+
+function route(app) {
+    app.get('/search', searchRouter);
+
+    app.get('/news', newsRouter);
+    app.get('/', siteRouter);
+}
+
+module.exports = route;
---
+++ src/routes/search.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const searchController = require('../app/controllers/SearchController');
+router.use('/', searchController.index);
+
+module.exports = router;
