fa9d876072447e0c095b3e5cd7b72f63d2322b78
minquan-alt <gameonlinepa@gmail.com>
2024-10-29 15:32:02 +0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const port = 3000;
 
 const route = require('./routes');
 
-          app.use(express.static(path.join(__dirname, 'public')));
+              app.use(express.static(path.join(__dirname, 'public')));
 
-      app.use(
-    express.urlencoded({
+            app.use(
+          express.urlencoded({
         extended: true,
     }),
     )     ;
