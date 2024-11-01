08da801555ff3513839fab32e0cdf453453f55ba
minquan-alt <gameonlinepa@gmail.com>
2024-11-01 20:27:59 +0700

    change env.example

--- src/config/config.js
+++ src/config/config.js
@@ -1,43 +1,28 @@
+require('dotenv').config()
 const config = {
     development: {
-        username: 'wanmin',
-        password: 'Quang123233',
-        database: 'travel_website',
-        server: 'wanmin.database.windows.net',
-        host: 'wanmin.database.windows.net',
-        dialect: 'mssql',
-        options: {
-            encrypt: true,
-            trustServerCertificate: false,
-        },
-        connectionTimeout: 30000,
-        logging: false,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        server: process.env.DB_SERVER,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        options: process.env.DB_OPTIONS,
+        connectionTimeout: Number(process.env.DB_CONNECTION_TIMEOUT),
     },
     test: {
-        username: 'wanmin',
-        password: 'Quang123233',
-        database: 'travel_website_test', // Use a test database if available
-        server: 'wanmin.database.windows.net',
-        dialect: 'mssql',
-        options: {
-            encrypt: true,
-            enableArithAbort: true,
-            trustServerCertificate: false,
-        },
-        connectionTimeout: 30000,
+        username: 'root',
+        password: null,
+        database: 'database_test',
+        server: '127.0.0.1',
+        dialect: 'mysql',
     },
     production: {
-        username: 'wanmin',
-        password: 'Quang123233',
-        database: 'travel_website_prod', // Use a production database if available
-        server: 'wanmin.database.windows.net',
-        dialect: 'mssql',
-        options: {
-            encrypt: true,
-            enableArithAbort: true,
-            trustServerCertificate: false,
-        },
-        connectionTimeout: 30000,
+        username: 'root',
+        password: null,
+        database: 'database_production',
+        server: '127.0.0.1',
+        dialect: 'mysql',
     },
 }
 
