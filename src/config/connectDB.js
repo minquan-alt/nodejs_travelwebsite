1a5dabbea207d860c9d4025c5568e8acbc7577d8
theomiiday <22521370@gm.uit.edu.vn>
2024-11-09 17:51:30 +0700

    chỉnh sửa app.scss, xóa script login, sửa script signin, forgot_password, reset_password

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,30 +1,31 @@
-const sql = require('mssql');
-const config = require('./config');
+const sql = require('mssql')
+const config = require('./config')
 
-const dbConfig = config.development;
+const dbConfig = config.development
 
 const sqlConfig = {
     user: dbConfig.username, // đổi thành 'user'
     password: dbConfig.password,
     database: dbConfig.database,
     server: dbConfig.host, // dùng 'server' thay cho 'host'
+    port: 3306,
     options: {
         encrypt: true,
         trustServerCertificate: false,
-        enableArithAbort: true
-    }
+        enableArithAbort: true,
+    },
     // connectionTimeout: dbConfig.connectionTimeout, // Nếu cần dùng connectionTimeout, đảm bảo nó có giá trị trong file config
-};
+}
 
 async function connectDB() {
     try {
-        const pool = await sql.connect(sqlConfig);
-        console.log('Connected to SQL Server');
-        return pool;
+        const pool = await sql.connect(sqlConfig)
+        console.log('Connected to SQL Server')
+        return pool
     } catch (err) {
-        console.error('Database connection failed:', err);
-        throw err;
+        console.error('Database connection failed:', err)
+        throw err
     }
 }
 
-module.exports = connectDB;
+module.exports = connectDB
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const routerAdmin = require('./admin')
 const routerWeb = require('./web')
-const routerShowForm = require
+//const routerShowForm = require
 
 let initWebRoutes = (app) => {
     app.use('/admin', routerAdmin)
     app.use('/', routerWeb)
 }
-
 
 // function route(app) {
 //     app.use('/api/v1/user', userRouter)
