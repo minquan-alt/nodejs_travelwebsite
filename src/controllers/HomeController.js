64b11bf9ec32251c0b22f3b73350c51e23d182e8
Thy <phuongthythythy@gmail.com>
2024-11-11 20:03:13 +0700

    create homepage

---
+++ src/controllers/HomeController.js
@@ -0,0 +1,25 @@
+const db = require('../models/index')
+
+let getHomePage = async (req, res) => {
+    try {
+        const data = await db.User.findAll()
+        return res.render('homepage', {
+            data: JSON.stringify(data),
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+let getRegisterPage = (req, res) => {
+    return res.render('register.js')
+}
+
+let getAboutPage = (req, res) => {
+    return res.render('test/about.ejs')
+}
+
+module.exports = {
+    getHomePage,
+    getAboutPage,
+    getRegisterPage,
+}
